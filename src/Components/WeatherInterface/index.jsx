c464a42424147498d35162e214e11ddda64b9a7b
L1ghtnessTL <shadowhunter07213@gmail.com>
2023-03-02 21:02:20 -0500

    Original Changes

---
+++ src/Components/WeatherInterface/index.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+export const WeatherInterface = ({
+    weatherData
+}) => {
+    return(
+        <div className='weatherbox'>
+            <div className='weatherbox-'>
+
+            </div>
+        </div>
+    )
+}
