702b643c64e0f61d3a90fb2cf7da51273951a9da
L1ghtnessTL <shadowhunter07213@gmail.com>
2023-03-09 20:32:19 -0500

    Final Changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import {useFetch}  from './Hooks/useFetch'
 import {WeatherInterface} from './Components/WeatherInterface'
 
 function App() {
-  const {data, isLoading} = useFetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=bbba429e9f1cc3cd79fa16b38ea8b050')
+  const {data, isLoading} = useFetch(`https://api.openweathermap.org/data/2.5/weather?q=Barranquilla&appid=bbba429e9f1cc3cd79fa16b38ea8b050`)
 
   if(isLoading) return <>Loading</>
   return (
-    <>
-      {data?.base}
+    <div className='container'>
       <WeatherInterface weatherData={data}/>
-    </>
+    </div>
   );
 }
 
